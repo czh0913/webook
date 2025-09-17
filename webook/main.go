a95ed1ef89bdbcc3a01ea4a78a1cced1ee20f6e0
chhh31 <3151979941@qq.com>
2025-09-17 14:59:12 +0800

    测试dao

--- webook/main.go
+++ webook/main.go
@@ -2,11 +2,6 @@ package main
 
 func main() {
 
-	//server := initWebServer()
-	//db := initDB()
-	//rdb := initRedis()
-	//u := initUser(db, rdb)
-	//u.RegisterRoutes(server)
 	server := InitWebServer()
 
 	server.Run("0.0.0.0:8080")
