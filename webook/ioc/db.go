6a61e2cad367371778a1b6e2a28a153e1b4a0fff
程子豪 <chhh31@chengzihaodeMacBook-Air.local>
2025-09-25 20:40:44 +0800

    引入viper

--- webook/ioc/db.go
+++ webook/ioc/db.go
@@ -1,14 +1,28 @@
 package ioc
 
 import (
-	"github.com/czh0913/gocode/basic-go/webook/config"
 	"github.com/czh0913/gocode/basic-go/webook/internal/repository/dao"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
+
+	type Config struct {
+		Dsn string `yaml:"dsn"`
+	}
+	var cfg Config
+	cfg = Config{
+		Dsn: "root:root@tcp(localhost:3306)/webook",
+	}
+	err := viper.UnmarshalKey("db", &cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(mysql.Open(cfg.Dsn))
+	//db, err := gorm.Open(mysql.Open(config.Config.DB.DSN))
 	if err != nil {
 		//只会在初始化过程种 panic
 		//整个 goroutine 结束
--- webook/main.go
+++ webook/main.go
@@ -1,8 +1,43 @@
 package main
 
-func main() {
+import (
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
 
+func main() {
+	initViperV1()
 	server := InitWebServer()
 
 	server.Run("0.0.0.0:8080")
+}
+
+func initViperV1() {
+	cfile := pflag.String("config", "config/config.yaml", "指定配置文件路径")
+	pflag.Parse()
+	viper.SetConfigFile(*cfile)
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func initViper() {
+
+	viper.SetConfigName("dev")
+	viper.SetConfigType("yaml")
+
+	viper.AddConfigPath("./config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	//otherviper := viper.New()
+	//otherviper.SetConfigName("myjson")
+	//otherviper.AddConfigPath("./config")
+	//otherviper.SetConfigType("json")
+
 }
